ab1b7b409013caae05e64f1963a41f096a8ab61a
Andrei Nicolae <andreicalutiu@gmail.com>
2025-04-30 17:13:39 +0300

    home component designed

--- src/app/components/shared/home/home.component.ts
+++ src/app/components/shared/home/home.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateToCalculator(): void {
+    this.router.navigate(['/calculator']);
+  }
+
+  navigateToSimulations(): void {
+    this.router.navigate(['/simulations']);
+  }
 }
