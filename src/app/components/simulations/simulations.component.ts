f7cedba62cecbfe50e8ce765b49f67523319a47f
Andrei Nicolae <andreicalutiu@gmail.com>
2025-05-01 10:57:25 +0300

    simulations page to retrieve all the calculations from localstorage

--- src/app/components/simulations/simulations.component.ts
+++ src/app/components/simulations/simulations.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoanSimulation, LoanType } from '../../models/loan.model';
+import { LoanService } from '../../services/loan.service';
 
 @Component({
   selector: 'app-simulations',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe, CommonModule],
   templateUrl: './simulations.component.html',
   styleUrl: './simulations.component.css'
 })
 export class SimulationsComponent {
+  simulations: LoanSimulation[] = [];
 
+  constructor(private loanService: LoanService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loanService.getSimulations().subscribe(simulations => {
+      this.simulations = simulations.sort((a, b) => {
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+      });
+    });
+  }
+
+  navigateToCalculator(): void {
+    this.router.navigate(['/calculator']);
+  }
+
+  getLoanTypeName(type: LoanType): string {
+    switch (type) {
+      case LoanType.SIMPLE:
+        return 'Simple Loan';
+      case LoanType.COMPOUND:
+        return 'Compound Loan';
+      case LoanType.LEASING:
+        return 'Leasing';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  deleteSimulation(id: string, event: Event): void {
+    event.stopPropagation();
+    if (confirm('Are you sure you want to delete this simulation?')) {
+      this.loanService.deleteSimulation(id);
+    }
+  }
 }
